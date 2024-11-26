64822072c8d40aa918b8c343015af45979951a16
UKLON\illia.koval <illia.koval@uklon.com.ua>
2024-11-26 23:46:28 +0200

    DEL-4: added 12-th lesson

---
+++ TestProjectPopeliuhaAutomation/Lesson12.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace TestProjectPopeliuhaAutomation;
+
+public class Lesson12
+{
+    class TestClassLesson12
+    {
+        [Test]
+        public void TestKeyClass()
+        {
+            var driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("https://mta.ua/");
+
+            IWebElement txtSearchField = driver.FindElement(By.XPath("//input[@name='search']"));
+            // txtSearchField.SendKeys("Привіт!");
+            // txtSearchField.SendKeys(Keys.Backspace);
+            // txtSearchField.SendKeys(Keys.Enter);
+            txtSearchField.SendKeys(Keys.Shift+"a");
+
+        }
+    }
+}
