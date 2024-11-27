8cf34e21c4bc221a699ec8e0ea1e003868c712e1
UKLON\illia.koval <illia.koval@uklon.com.ua>
2024-11-27 21:43:59 +0200

    DEL-4:added 13 lesson+14

---
+++ TestProjectPopeliuhaAutomation/Lesson13.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace TestProjectPopeliuhaAutomation;
+
+public class Lesson13
+{
+    class TestClassLesson13
+    {
+        [Test]
+        public void TestGetAttributeAndText_FromElementOnPage()
+        {
+            var driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("https://mta.ua/telefoni-ta-smartfoni");
+
+            IWebElement txtHeader = driver.FindElement(By.XPath("/html/body/div[4]/div/div[1]/main/div/div[2]/div/div[1]/div/div[3]/a"));
+            string headerText = txtHeader.Text;
+            Console.WriteLine($"headerText is : {headerText}");
+            string headerClass = txtHeader.GetAttribute("class");//якщо невірно вказати назву атрибуту-- помилки не буде, буде пустий вивід
+            Console.WriteLine($"headerClass is : {headerClass}");
+            
+            driver.Close();
+
+            // IWebElement searchTitle =
+            //     driver.FindElement(By.XPath("/html/body/div[4]/div/div[1]/main/div/div[2]/div/div[1]/div/div[3]/a"));
+            // string searchTestTitle = searchTitle.GetAttribute("title");
+            // Console.WriteLine($"i search : {searchTestTitle}");
+        }
+    }
+
+}
